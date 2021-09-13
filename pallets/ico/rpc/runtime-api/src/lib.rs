8ad97849c61a2750b5cccaee1e2dd524d9cd02d9
barbara <dicobarbara@protonmail.com>
2021-09-13 17:47:27 +0800

    feat(ico): Add the ico module

---
+++ pallets/ico/rpc/runtime-api/src/lib.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+sp_api::decl_runtime_apis! {
+	pub trait IcoAmountApi<AccountId, CurrencyId, Index, Balance> where
+		AccountId: codec::Codec,
+		CurrencyId: codec::Codec,
+		Index: codec::Codec,
+		Balance: codec::Codec,
+	{
+		fn can_release_amount(account: AccountId, currency_id: CurrencyId, index: Index) -> Balance;
+		fn get_reward_amount(account: AccountId, currency_id: CurrencyId, index: Index) -> Balance;
+		fn can_unlock_amount(user: AccountId, currency_id: CurrencyId, index: Index) -> Balance;
+	}
+
+}
---
+++ pallets/ico/src/traits.rs
@@ -0,0 +1,9 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use sp_std::{prelude::*, result};
+pub trait IcoHandler<CurrencyId, MulBalanceOf, AccountId, DispathErr, BlockNumber> {
+
+    fn is_project_ico_member(currency_id: CurrencyId, index: u32, who: &AccountId) -> result::Result<bool, DispathErr>;
+    fn get_user_total_amount(currency_id: CurrencyId, index: u32, who: &AccountId) -> MulBalanceOf;
+    fn get_project_total_ico_amount(currency_id: CurrencyId, index: u32) -> result::Result<MulBalanceOf, DispathErr>;
+}
