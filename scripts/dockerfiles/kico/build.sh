8bd86a6c242e74b53eed8dc405596e50859e38a4
cj1afs <cj1afs@outlook.com>
2021-11-22 17:49:04 +0800

    update kico-config.yml

--- scripts/dockerfiles/kico/build.sh
+++ scripts/dockerfiles/kico/build.sh
@@ -16,7 +16,7 @@ GITREPO=kico
 
 # Build the image
 echo "Building ${GITUSER}/${GITREPO}:latest docker image, hang on!"
- time docker build -f ./scripts/dockerfiles/kico/kico_builder.Dockerfile -t ${GITUSER}/${GITREPO}:latest .
+time docker build -f ./scripts/dockerfiles/kico/kico_builder.Dockerfile -t ${GITUSER}/${GITREPO}:latest .
 docker tag ${GITUSER}/${GITREPO}:latest ${GITUSER}/${GITREPO}:v${VERSION}
 
 # Show the list of available images for this repo
