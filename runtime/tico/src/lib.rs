44b88032246ace80893d765c197dcd8cf1d2daf7
cj1afs <cj1afs@outlook.com>
2022-02-20 01:22:25 +0800

    update spec_version

--- runtime/tico/src/lib.rs
+++ runtime/tico/src/lib.rs
@@ -123,7 +123,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("TICO"),
 	impl_name: create_runtime_str!("TICO"),
 	authoring_version: 1,
-	spec_version: 1200,
+	spec_version: 1210,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
