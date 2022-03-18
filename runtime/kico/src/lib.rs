5f36be750f50d8652c3729d1ab3eea380d135271
cj1afs <cj1afs@outlook.com>
2022-03-18 12:28:40 +0800

    update spec version

--- runtime/kico/src/lib.rs
+++ runtime/kico/src/lib.rs
@@ -128,7 +128,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("KICO"),
 	impl_name: create_runtime_str!("KICO"),
 	authoring_version: 1,
-	spec_version: 1210,
+	spec_version: 1220,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
