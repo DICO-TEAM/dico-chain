17346af00c83483ac3c07266081f57f5a0c8f65f
cj1afs <89183120+cj1afs@users.noreply.github.com>
2022-04-02 10:24:08 +0800

    add acala usdt (#197)

--- runtime/kico/src/constants.rs
+++ runtime/kico/src/constants.rs
@@ -4,6 +4,7 @@ pub mod paras {
     // Karura native tokens
     pub mod karura {
         pub const ID: u32 = 2000;
+        pub const AUSD_KEY: &[u8] = &[0, 1];
         pub const KAR_KEY: &[u8] = &[0, 128];
         pub const KUSD_KEY: &[u8] = &[0, 129];
         pub const LKSM_KEY: &[u8] = &[0, 131];
