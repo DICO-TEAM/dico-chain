e1aaba838e6a2cd573039f017dcd7cd9fccee4c0
wetalice <wetalice@protonmail.com>
2022-04-09 17:36:51 +0800

    updata string len

--- pallets/dao/src/benchmarking.rs
+++ pallets/dao/src/benchmarking.rs
@@ -55,7 +55,7 @@ fn set_propose<T: Config>() -> (T::Hash, u32, u32) {
 	let currency_id = 1;
 	let ico_index = 1;
 	T::IcoHandler::set_ico_for_bench(currency_id, ico_index, caller.clone(), get_alice::<T>(), get_bob::<T>());
-	let proposal: T::Proposal = SystemCall::<T>::remark { remark: vec![1; 3] }.into();
+	let proposal: T::Proposal = SystemCall::<T>::remark { remark: vec![1; 100] }.into();
 	let hash = T::Hashing::hash_of(&proposal);
 
 	assert!(Dao::<T>::propose(
@@ -64,7 +64,7 @@ fn set_propose<T: Config>() -> (T::Hash, u32, u32) {
 		ico_index,
 		Percent::from_percent(30u8),
 		Box::new(proposal),
-		vec![1; 3],
+		vec![1; 100],
 		100
 	)
 	.is_ok());
@@ -75,12 +75,12 @@ fn set_propose<T: Config>() -> (T::Hash, u32, u32) {
 benchmarks! {
 	propose {
 		let caller: T::AccountId = whitelisted_caller();
-		let proposal: T::Proposal = SystemCall::<T>::remark { remark: vec![1; 3] }.into();
+		let proposal: T::Proposal = SystemCall::<T>::remark { remark: vec![1; 100] }.into();
 		let hash = T::Hashing::hash_of(&proposal);
 		let currency_id = 1;
 		let ico_index = 1;
 		T::IcoHandler::set_ico_for_bench(currency_id, ico_index, caller.clone(), get_alice::<T>(), get_bob::<T>());
-	}:_(RawOrigin::Signed(get_alice::<T>()), currency_id, 1, Percent::from_percent(30u8), Box::new(proposal), vec![1;3], 100)
+	}:_(RawOrigin::Signed(get_alice::<T>()), currency_id, 1, Percent::from_percent(30u8), Box::new(proposal), vec![1;100], 100)
 
 	vote {
 		let caller: T::AccountId = get_bob::<T>();
