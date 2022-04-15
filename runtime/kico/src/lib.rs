3827a6bea635723e84e7a7cccff42618e75df647
cj1afs <cj1afs@outlook.com>
2022-04-15 20:15:51 +0800

    update kyc migration logic

--- runtime/kico/src/lib.rs
+++ runtime/kico/src/lib.rs
@@ -108,7 +108,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	KYCMigrationV2,
+	(),
 >;
 
 pub struct KYCMigrationV2;
