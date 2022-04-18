7b05975081008bc99dad07f62d2f407930f1ce39
cj1afs <89183120+cj1afs@users.noreply.github.com>
2022-04-18 16:50:13 +0800

    release v1.3.0 (#217)

--- runtime/kico/src/lib.rs
+++ runtime/kico/src/lib.rs
@@ -141,7 +141,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("KICO"),
 	impl_name: create_runtime_str!("KICO"),
 	authoring_version: 1,
-	spec_version: 1260,
+	spec_version: 1300,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
