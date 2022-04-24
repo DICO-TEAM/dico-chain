3ab4735b34a9c399dbcf75f282086d3257355b2a
cj1afs <cj1afs@outlook.com>
2022-04-25 02:37:06 +0800

    release v1.3.1

--- runtime/tico/src/lib.rs
+++ runtime/tico/src/lib.rs
@@ -142,7 +142,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("TICO"),
 	impl_name: create_runtime_str!("TICO"),
 	authoring_version: 1,
-	spec_version: 1300,
+	spec_version: 1310,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
