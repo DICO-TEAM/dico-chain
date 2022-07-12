a597e657878780eeba594bc249a622c1618ce656
wetalice <wetalice@protonmail.com>
2022-07-12 11:42:41 +0800

    update tico runtime/lib

---
+++ runtime/tico/src/migrations.rs
@@ -0,0 +1,19 @@
+
+use super::*;
+pub struct KYCMigrationV2;
+
+impl OnRuntimeUpgrade for KYCMigrationV2 {
+	fn on_runtime_upgrade() -> frame_support::weights::Weight {
+		pallet_kyc::migrations::v2::migrate::<Runtime>()
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn pre_upgrade() -> Result<(), &'static str> {
+		pallet_kyc::migrations::v2::pre_migrate::<Runtime>()
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn post_upgrade() -> Result<(), &'static str> {
+		pallet_kyc::migrations::v2::post_migrate::<Runtime>()
+	}
+}
