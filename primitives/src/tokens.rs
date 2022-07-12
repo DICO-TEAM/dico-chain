0e700053675bfb27443e69c60ad3bb36fe0aa59e
wetalice <wetalice@protonmail.com>
2022-07-12 22:00:49 +0800

    delete irrelevant assets

--- primitives/src/tokens.rs
+++ primitives/src/tokens.rs
@@ -9,10 +9,7 @@ pub const AUSD: CurrencyId = 10;
 
 // substrate ecosystem
 pub const KSM: CurrencyId = 100;
-pub const DOT: CurrencyId = 101;
 pub const KAR: CurrencyId = 102;
-pub const ACA: CurrencyId = 103;
 pub const LKSM: CurrencyId = 104;
-pub const LDOT: CurrencyId = 105;
 
 
