79d46b08821ab7ad2ade519d924a4812014ba051
wetalice <wetalice@protonmail.com>
2022-07-19 14:29:46 +0800

    fix(xcm): modify the listen assets's id

--- runtime/tico/src/parachains.rs
+++ runtime/tico/src/parachains.rs
@@ -3,11 +3,11 @@ use super::*;
 pub mod listen {
 		pub const PARA_ID: u32 = 2022;
 		pub mod like {
-			pub const ASSET_ID: u32 = 5;
+			pub const ASSET_ID: u32 = 11;
 			pub const TOKEN_SYMBOL: &[u8] = "LIKE".as_bytes();
 		}
 		pub mod lt {
-			pub const ASSET_ID: u32 = 6;
+			pub const ASSET_ID: u32 = 12;
 			pub const TOKEN_SYMBOL: &[u8] = "LT".as_bytes();
 		}
 	}
