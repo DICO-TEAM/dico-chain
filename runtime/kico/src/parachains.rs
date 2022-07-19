e7c8395687168b7ee4dcc348137ccf707916c454
wetalice <wetalice@protonmail.com>
2022-07-19 14:33:08 +0800

    fix(xcm): modify the listen LIKE asset's id

--- runtime/kico/src/parachains.rs
+++ runtime/kico/src/parachains.rs
@@ -2,7 +2,7 @@
 pub mod listen {
 		pub const PARA_ID: u32 = 2118;
 		pub mod like {
-			pub const ASSET_ID: u32 = 11;
+			pub const ASSET_ID: u32 = 13;
 			pub const TOKEN_SYMBOL: &[u8] = "LIKE".as_bytes();
 		}
 		pub mod lt {
--- runtime/tico/src/parachains.rs
+++ runtime/tico/src/parachains.rs
@@ -3,7 +3,7 @@ use super::*;
 pub mod listen {
 		pub const PARA_ID: u32 = 2022;
 		pub mod like {
-			pub const ASSET_ID: u32 = 11;
+			pub const ASSET_ID: u32 = 13;
 			pub const TOKEN_SYMBOL: &[u8] = "LIKE".as_bytes();
 		}
 		pub mod lt {
