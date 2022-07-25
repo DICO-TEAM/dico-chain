fd2010a4808151f666a40165028b95bd0ec66a20
wetalice <wetalice@protonmail.com>
2022-07-25 21:28:51 +0800

    update

--- runtime/kico/src/lib.rs
+++ runtime/kico/src/lib.rs
@@ -142,7 +142,7 @@ pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signatu
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, Call, SignedExtra>;
 /// Executive: handles dispatch to the various modules.
 pub type Executive =
-	frame_executive::Executive<Runtime, Block, frame_system::ChainContext<Runtime>, Runtime, AllPalletsWithSystem, (ResetTechnicalCommitteeMember, MigrationOPVesting)>;
+	frame_executive::Executive<Runtime, Block, frame_system::ChainContext<Runtime>, Runtime, AllPalletsWithSystem, ()>;
 
 impl_opaque_keys! {
 	pub struct SessionKeys {
