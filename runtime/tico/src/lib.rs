e310cea73dc3ca752057647de60f41c11f77c7d2
wetalice <wetalice@protonmail.com>
2022-09-17 11:03:56 +0800

    update

--- runtime/tico/src/lib.rs
+++ runtime/tico/src/lib.rs
@@ -151,7 +151,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("TICO"),
 	impl_name: create_runtime_str!("TICO"),
 	authoring_version: 1,
-	spec_version: 2022091601,
+	spec_version: 2022091701,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
